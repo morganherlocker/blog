af46029409b3e4f0578515512e9160ebdcb195cc
Morgan Herlocker <morgan.herlocker@gmail.com>
2013-11-25 16:28:59 -0500

    blog working with most recent on index

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var fs = require('fs'),
+    path = require('path'),
+    marked = require('marked')
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  // get first 5 posts
+  fs.readdir(__dirname+'/../posts/', function(err, files){
+    var recentName = files[files.length - 1]
+    fs.readFile(__dirname+'/../posts/'+recentName, 'utf8', function(err, content){
+      var htmlContent = marked(content)
+      res.render('index', { title: 'Express', content: htmlContent});
+    })
+
+  })
+};
