daec5dd14715996d088b7e832c85c0cd1a15095f
Morgan Herlocker <morgan.herlocker@gmail.com>
2013-12-02 16:26:47 -0500

    updated to using badblog

--- blog/app.js
+++ blog/app.js
@@ -1,5 +1,4 @@
 var express = require('express')
-  , index = require('./routes/index')
   , posts = require('./routes/posts')
   , post = require('./routes/post')
   , http = require('http')
@@ -9,7 +8,7 @@ var express = require('express')
 
 var app = express();
 
-// all environment
+// all environments
 app.set('port', process.env.PORT || 80);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
@@ -29,21 +28,25 @@ app.get('/', function(req, res){res.redirect('/post/recent')});
 app.get('/posts', posts)
 app.get('/post/:name', post)
 
+config = {}
 postInfos = []
 var i = 1
-fs.readdir(__dirname+'/posts/', function(err, files){
-  _.each(files, function(file){
-    fs.readFile(__dirname+'/posts/'+file, 'utf8', function(err, content){
-      var title = content.split('===')[0].replace(/(\r\n|\n|\r)/gm,"")
-      var url = title.split(' ').join('-').replace(/(\r\n|\n|\r)/gm,"")
-      postInfos.push({id: file, title: title, url: url})
-      if(i >= files.length){
-        postInfos = _.sortBy(postInfos, 'id')
-        http.createServer(app).listen(app.get('port'), function(){
-          console.log('Express server listening on port ' + app.get('port'));
-        });
-      }
-      i++
+fs.readFile(__dirname+'/../config.json', function(err, data){
+  if(err) throw err
+  config = JSON.parse(data)
+  fs.readdir(__dirname+'/../posts/', function(err, files){
+    _.each(files, function(file){
+      fs.readFile(__dirname+'/../posts/'+file, 'utf8', function(err, content){
+        var title = content.split('===')[0].replace(/(\r\n|\n|\r)/gm,"")
+        var url = title.split(' ').join('-').replace(/(\r\n|\n|\r)/gm,"")
+        postInfos.push({id: file, title: title, url: url})
+        if(i >= files.length){
+          http.createServer(app).listen(app.get('port'), function(){
+            console.log('Express server listening on port ' + app.get('port'));
+          });
+        }
+        i++
+      })
     })
   })
-})
+}) 
