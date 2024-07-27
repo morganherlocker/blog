99e3c274943c35157ad50066d284f63d51c57e1b
Morgan Herlocker <morgan.herlocker@gmail.com>
2024-07-26 23:10:44 -0700

    var -> const/let, rm analytics, update bio

--- build.js
+++ build.js
@@ -1,27 +1,27 @@
-var glob = require('glob')
-var fs = require('fs')
-var rm = require('rimraf').sync
-var marked = require('marked')
-var _ = require('lodash')
+const glob = require('glob')
+const fs = require('fs')
+const rm = require('rimraf').sync
+const marked = require('marked')
+const _ = require('lodash')
 
-var template = fs.readFileSync('./views/template', 'utf8')
+const template = fs.readFileSync('./views/template', 'utf8')
 
-var pages = glob.sync('./pages/*')
+const pages = glob.sync('./pages/*')
 pages.forEach(path => {
-  var name = path.split('/')[path.split('/').length-1]
-  var content = fs.readFileSync(path, 'utf8')
-  var content = template.split('{content}').join(content)
+  const name = path.split('/')[path.split('/').length-1]
+  let content = fs.readFileSync(path, 'utf8')
+  content = template.split('{content}').join(content)
   rm('./'+name)
   fs.mkdirSync('./'+name)
   fs.writeFileSync('./'+name+'/index.html', content)
 })
 
-var drafts = glob.sync('./drafts/*').map(d => {
-  var file = d.split('/')[d.split('/').length-1]
-  var copy = fs.readFileSync(d, 'utf8')
-  var name = copy.split('\n')[0]
-  var url = name.split(' ').join('-')
-  var html = template.split('{content}').join(marked(copy))
+let drafts = glob.sync('./drafts/*').map(d => {
+  const file = d.split('/')[d.split('/').length-1]
+  const copy = fs.readFileSync(d, 'utf8')
+  const name = copy.split('\n')[0]
+  const url = name.split(' ').join('-')
+  const html = template.split('{content}').join(marked(copy))
   return {
     name: name,
     content: html,
@@ -33,7 +33,7 @@ var drafts = glob.sync('./drafts/*').map(d => {
 drafts = _.sortBy(drafts, 'file').reverse()
 
 rm('./posts')
-var postsList = '<hr>'
+let postsList = '<hr>'
 drafts.forEach(d => {
   postsList += '<p><a href="/post/'+d.url+'">'+d.name+'</a>'
 })
@@ -50,16 +50,17 @@ drafts.forEach(d => {
 
 fs.writeFileSync('./index.html', _.first(drafts).content)
 
+fs.openSync('./rss.xml', 'w');
 fs.unlinkSync('./rss.xml')
-var rss = ''
+let rss = ''
 rss += '<rss xmlns:atom="http://www.w3.org/2005/Atom" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:webfeeds="http://webfeeds.org/rss/1.0" version="2.0">'
 rss += '<channel>'
 rss += '<title>morganherlocker</title>'
 rss += '<link>http://morganherlocker.com</link>'
 rss += '<description>website of @morganherlocker</description>'
 drafts.slice(0,5).forEach(d => {
-  var body = marked(d.copy.split('===').slice(1).join(''))
-  var date = d.file.split('--')[0].split('-')
+  const body = marked(d.copy.split('===').slice(1).join(''))
+  let date = d.file.split('--')[0].split('-')
   date = new Date(date[1]+'/'+date[2]+'/'+date[0]).toUTCString()
   rss += '<item>'
   rss += '<title>' + d.name + '</title>'
